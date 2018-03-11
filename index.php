08f60dd2190e97d126a370fe5772e0b4cfdad7ba
Siam Hossain <siam.hossain1994@gmail.com>
2018-03-11 14:53:54 +0600

    Shooww images in index page

---
+++ index.php
@@ -0,0 +1,7 @@
+<?php
+    $images = array("1.jpg", "2.jpg", "3.jpg", "4.jpg", "5.jpg");
+    foreach($images as $image){
+        echo "<img src='images/" . $image . "' width = '23%' height = '300' />\n";
+    }
+?>
+
