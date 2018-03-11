af3a22601e9ed6eca66fdc3f5a3d8376a4f94f1f
Siam Hossain <siam.hossain1994@gmail.com>
2018-03-11 14:57:32 +0600

    Show images in random order

--- index.php
+++ index.php
@@ -1,5 +1,6 @@
 <?php
     $images = array("1.jpg", "2.jpg", "3.jpg", "4.jpg", "5.jpg");
+    shuffle($images);
     foreach($images as $image){
         echo "<img src='images/" . $image . "' width = '23%' height = '300' />\n";
     }
